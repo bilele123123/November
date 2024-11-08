baa8971784e19da4b72351634dd718b64c6c723b
Thai Le <tple06203@gmail.com>
2024-11-08 00:45:50 -0800

    Fix deployment complications

--- app/birthday/page.tsx
+++ app/birthday/page.tsx
@@ -7,7 +7,7 @@ export default function Birthday() {
       <div className="flex-col m-4">
         <p className="text-3xl font-bold mt-2 mb-5">❤️Happy Birthday❤️</p>
         <p className="text-2xl font-bold mt-2 mb-5">❤️To my baby❤️</p>
-        <p className="mb-3">You’re the most brilliant and hardworking person I know, and your kindness makes the world a better place.</p>
+        <p className="mb-3">You are the most brilliant and hardworking person I know, and your kindness makes the world a better place.</p>
         <p className="mb-3">Every day, you inspire me with your intelligence and determination. I’m so lucky to have someone as thoughtful and caring as you by my side.</p>
         <p className="mb-3">You light up my life with your kindness and dedication, and I am constantly amazed by how effortlessly you make everything around you beautiful.</p>
         <p className="mb-3">You are not only incredibly smart and driven, but your warmth and compassion touch everyone you meet.</p>
